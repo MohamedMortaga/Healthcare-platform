a2c4416b504e7fb65d4ad63118a5e8736631354d
Youssef-Mekkkawy <31347791+Youssef-Mekkkawy@users.noreply.github.com>
2026-08-02 17:25:58 +0300

    new updaste
    update our proejct depnd on docuemnt file that mahmoud send to us

---
+++ .next/dev/static/chunks/app_page_tsx_0zr2k9a._.js
@@ -0,0 +1,5 @@
+(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: ["static/chunks/src_1ysocno._.js","static/chunks/node_modules_0nfh_88._.js"],
+    source: "dynamic"
+});
